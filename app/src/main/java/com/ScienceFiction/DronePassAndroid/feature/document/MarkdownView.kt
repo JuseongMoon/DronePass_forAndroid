b6f841afde8620f598651515c059c862322413bd
JuseongMoon <37950716+JuseongMoon@users.noreply.github.com>
2026-06-09 16:31:55 +0900

    fix: render markdown table dividers vertically

--- app/src/main/java/com/ScienceFiction/DronePassAndroid/feature/document/MarkdownView.kt
+++ app/src/main/java/com/ScienceFiction/DronePassAndroid/feature/document/MarkdownView.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -257,7 +258,7 @@ private fun VerticalDivider(color: Color) {
     Spacer(
         modifier = Modifier
             .width(0.5.dp)
-            .fillMaxWidth()
+            .fillMaxHeight()
             .background(color),
     )
 }
